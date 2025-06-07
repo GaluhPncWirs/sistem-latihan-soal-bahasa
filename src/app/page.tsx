7e9f861360062abc6dde425a189614bd14b3d99b
galuh panca wirasa <Galuh panca wirasa>
2025-06-07 11:48:05 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,66 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="bg-slate-300 h-screen flex justify-center items-center">
+      <div className="bg-slate-200 w-2/5 h-3/5 rounded-xl shadow-lg shadow-slate-400">
+        <h1 className="text-3xl font-bold text-blue-500 text-center mt-6 mb-4">
+          Login Akun
+        </h1>
+        <form className="flex justify-center h-2/3">
+          <div className="flex flex-col w-3/4 justify-around">
+            <label
+              htmlFor="email"
+              className="text-xl font-semibold text-blue-500"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="adamJobs@gmail.com"
+              className="w-full rounded-md p-3"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <label
+              htmlFor="password"
+              className="text-xl font-semibold text-blue-500"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="**********"
+              className="w-full rounded-md p-3"
+            />
+            <button
+              className="bg-amber-400 rounded-md w-full mt-3 hover:bg-amber-500 h-10 disabled:cursor-not-allowed"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="ml-10 mt-2.5 text-slate-700">
+          <button>
+            Masuk Dengan Akun{" "}
+            <span className="font-semibold hover:underline text-blue-700 cursor-pointer">
+              Google
+            </span>
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mt-10 text-center">
+          <p>
+            Belum Punya Akun ?{" "}
+            <Link
+              href="#"
+              className="font-semibold text-blue-700 hover:underline"
+            >
+              Register
+            </Link>{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
