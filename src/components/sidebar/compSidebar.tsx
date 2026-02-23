923f163f8e1dec627b10764b905ddb89817cf1da
galuh panca wirasa <galuhpanca166@gmail.com>
2026-02-23 11:04:33 +0700

    memperbaiki eror di vercel

--- src/components/sidebar/compSidebar.tsx
+++ src/components/sidebar/compSidebar.tsx
@@ -3,21 +3,15 @@ import ListSidebar from "./listSidebar/content";
 import { useHandleClickedHamburgerMenu } from "@/app/hooks/getHandleHamMenu";
 
 export default function HamburgerMenuBar() {
-  const {
-    isCheked,
-    clickOutsideHamburgerMenu,
-    clickOutsidePath,
-    setIsCheked,
-    setIsTrue,
-  } = useHandleClickedHamburgerMenu();
+  const { isCheked, clickOutsidePath, setIsCheked, setIsTrue } =
+    useHandleClickedHamburgerMenu();
 
   return (
-    <div className="md:hidden">
+    <div className="md:hidden" ref={clickOutsidePath}>
       <div className="menu flex flex-col items-center gap-y-1">
         <Input
           type="checkbox"
           className="absolute size-6 cursor-pointer z-20 opacity-0"
-          ref={clickOutsideHamburgerMenu}
           checked={isCheked}
           onChange={() => {
             setIsCheked((prev) => !prev);
@@ -32,7 +26,6 @@ export default function HamburgerMenuBar() {
       <div
         className={`flex fixed shadow-xl shadow-slate-700 left-0 top-0 flex-col bg-[#476EAE] items-center w-64 transition-all duration-300 text-xl font-semibold h-screen z-50 pt-14
       ${isCheked ? `translate-x-0` : `-translate-x-full`}`}
-        ref={clickOutsidePath}
       >
         <ListSidebar />
       </div>
